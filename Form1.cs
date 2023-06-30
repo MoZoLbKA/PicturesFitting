01f14260f7bd9ab1b07cabb2aa1663c096ddd5b5
MoZoLbKA <makurin.nik@list.ru>
2023-06-30 12:42:19 +0300

    1 пункт задачи(строка с шириной и одинаковой высотой)

--- Form1.cs
+++ Form1.cs
@@ -13,13 +13,13 @@ namespace PicturesFitting
 {
     public partial class Form1 : Form
     {
+        Row row1 = new Row();
         public Form1()
         {
             InitializeComponent();
-            Row row1 = new Row();
             row1.Add("1.jpg").Add("2.jpg").Add("3.jpg");
-            row1.RelocatePictures();
-            pictureBox1.Image = row1.compiledRow;
+            pictureBox1.Image = row1.GetTreeImages();
+             
 
         }
 
@@ -34,6 +34,12 @@ namespace PicturesFitting
             {
                 MessageBox.Show("Нет картинки");
             }
+        }
+
+        private void resizeButton_Click(object sender, EventArgs e)
+        {
+            int w = (int)numericUpDown1.Value;
+            pictureBox1.Image =  row1.ResizeImages(w);
         }
     }
 }
--- Row.cs
+++ Row.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -28,9 +29,14 @@ namespace PicturesFitting
             data.Add(ConvertToBitmap(frame));
             return this;
         }
-        public void RelocatePictures()
+        public Row Add(Column frame)
+        {
+            return this;
+        }
+        public Bitmap GetTreeImages()
         {
             compiledRow = MergeImages(data);
+            return compiledRow;
         }
 
         private Bitmap MergeImages(IEnumerable<Bitmap> images)
@@ -58,7 +64,83 @@ namespace PicturesFitting
                     localWidth += image.Width;
                 }
             }
+            compiledRow = bitmap;
             return bitmap;
+        }
+        private int Sum(List<int> array)
+        {
+            int sum = 0;
+            foreach (var item in array)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        private Bitmap ResizeImage(Bitmap img,Size size)
+        {
+            Bitmap b = new Bitmap(size.Width, size.Height);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(img, 0, 0, size.Width, size.Height);
+            }
+            return b;
+        }
+        private Bitmap Place(Bitmap bigBmp,Bitmap smallBmp,Point position)
+        {
+            Graphics g = Graphics.FromImage(bigBmp);
+            g.CompositingMode = CompositingMode.SourceCopy;
+            int x = bigBmp.Width - smallBmp.Width ;
+            int y = bigBmp.Height - smallBmp.Height;
+            g.DrawImage(smallBmp, new Point(x, y));
+            return bigBmp;
+        }
+        internal Bitmap ResizeImages(int w)
+        {
+            if(data == null)
+            {
+                return null;
+            }
+            List<int> heights =new List<int>(data.Count);
+            List<int> widths =new List<int>(data.Count);
+            for (int i = 0; i < data.Count; i++)
+            {
+                heights.Add(data[i].Height);
+                widths.Add(data[i].Width);
+            }
+            while (Sum(widths) > w)
+            {
+                int sumOfWidths = Sum(widths);
+                int sumOfHeight = Sum(heights);
+                if (sumOfWidths > w)
+                {
+                    double coeff = (double)sumOfWidths / w;
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        double ratio = widths[i] / heights[i];
+                        widths[i] = (int)(widths[i] / coeff);
+                        heights[i] = (int)(widths[i] / ratio);
+                    }
+
+                }
+                    double min = heights.Min();
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        double ratio = (double)heights[i] / widths[i];
+                        int redusion = (int)(heights[i] - min);
+                        heights[i] -= redusion;
+                        widths[i] = (int)(heights[i] / ratio);
+
+                    }
+            }
+            List<Bitmap> compression = new List<Bitmap>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                compression.Add(ResizeImage(data[i], new Size(widths[i],heights[i])));
+            }
+            Bitmap tmp = MergeImages(compression);
+            return ResizeImage(tmp, new Size(w, (tmp.Width / (tmp.Width/tmp.Height ))));
+
         }
     }
 }
